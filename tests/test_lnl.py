01b17ecf56b1116466de0327694794cfde403018
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-09-06 00:53:09 +1200

    setup

--- tests/test_lnl.py
+++ tests/test_lnl.py
@@ -1,8 +1,14 @@
 
+def test_lnl(var2_data):
+    #TODO: implement this test
 
-def test_lnl():
-    pass
+    # compute LNL (original, no blocks)
 
+    # compute LNL (with 1 block)
 
-def test_block_lnl():
+    # compute LNL (with 2 blocks)
+
+    # assert that Lnl with 2 blocks is less than Lnl with 1 block
+    # assert that Lnl with 1 block ==  Lnl with no blocks
+
     pass
