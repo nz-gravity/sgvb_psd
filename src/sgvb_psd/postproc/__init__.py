ae5bd4d0967c5c6d4c38ccb86331ddad329cbaa9
avi <avi.vajpeyi@gmail.com>
2024-09-09 15:40:42 +1200

    hacking with jianan

--- src/sgvb_psd/postproc/__init__.py
+++ src/sgvb_psd/postproc/__init__.py
@@ -1,2 +1,9 @@
+from pathlib import Path
+import matplotlib
+
+# Absolute path to the top level of the repository
+rc_file = Path(__file__).resolve().absolute().parents[0] / "matplotlibrc"
+matplotlib.rc_file(rc_file)
+
 from .psd_analyzer import PSDAnalyzer
-from .plot_psd import plot_psdq, plot_peridogram
+from .plot_psd import plot_psdq, plot_peridogram, plot_single_psd, format_axes
