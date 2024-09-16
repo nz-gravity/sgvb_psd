3a8244021098a498bf285a6d71077e65117db730
ljn94408 <jliu812@aucklanduni.ac.nz>
2024-09-17 05:08:36 +1200

    set the x limit for test_ET

--- src/sgvb_psd/utils/periodogram.py
+++ src/sgvb_psd/utils/periodogram.py
@@ -3,30 +3,12 @@ from scipy import signal
 from typing import Tuple
 
 
-def get_chunked_median_periodogram(x, fs, n_chunks=1) -> Tuple[np.ndarray, np.ndarray]:
-    """Given a multivariate time series, return the periodogram."""
-    chunked_x = np.array(np.array_split(x, n_chunks))
-    _, n, p = chunked_x.shape
-
-    assert (
-        n > p
-    ), "The number of samples must be greater than the number of variables."
-
-    f = None
-    pdgm = np.zeros((n_chunks, n // 2, p, p))
-    for i in range(n_chunks):
-        pdgm[i], f = get_periodogram(x, fs)
-    pdgm = np.median(pdgm, axis=0)
-
-    assert pdgm.shape == (n // 2, p, p)
-    return pdgm, f
 
 
 def get_periodogram(x, fs, psd_scaling=1, **kwargs) -> Tuple[np.ndarray, np.ndarray]:
     n, p = x.shape
     x = x/psd_scaling
     periodogram = np.zeros((n // 2, p, p), dtype=complex)
-    f = np.zeros(n // 2)
     for row_i in range(p):
         for col_j in range(p):
             if row_i == col_j:
